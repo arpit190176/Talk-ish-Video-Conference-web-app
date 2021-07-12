351d725e4f707926ba6c5bc64f6483890273eebb
arpit190176 <82868238+arpit190176@users.noreply.github.com>
2021-07-12 23:38:09 +0530

    Added comments

--- server.js
+++ server.js
@@ -3,37 +3,44 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
-app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
     origin: '*'
   }
 });
+
+app.set("view engine", "ejs");  //Indicating express that ejs is being used
+
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
 
 app.use("/peerjs", peerServer);
-app.use(express.static("public"));
+app.use(express.static("public")); //pull client script from public folder
 
+//send user to the homepage 
 app.get("/", (req, rsp) => {
   
   rsp.render("homepage")
 });
 
-app.get("/newRoom",(req,rsp)=>{ //if a newroom has been requested then append random id to url and redirect
+ // newroom requested => then append random ID to url and redirect
+app.get("/newRoom",(req,rsp)=>{ 
   rsp.redirect(`/${uuidv4()}`);
 });
 
+//if a particular room is joined then render it
 app.get("/:room", (req, rsp) => {
   rsp.render("room", { roomId: req.params.room });
 });
 
+//user connecting to server
 io.on("connection", (socket) => {
+  //attempt to join the room
   socket.on("join-room", (roomId, userId, userName) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.join(roomId); //join the room
+    socket.to(roomId).broadcast.emit("user-connected", userId); //indicate other members of the room someone has joined
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
     });
