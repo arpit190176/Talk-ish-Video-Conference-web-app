de3c8196b0ab58eca32d19a553ba2712e0a5915b
arpit190176 <82868238+arpit190176@users.noreply.github.com>
2021-07-12 23:39:03 +0530

    Added comments

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
 const socket = io("/");
 const vidGrid = document.getElementById("video-grid");
-const myVid = document.createElement("video");
+const myVid = document.createElement("video");//creating new video tag to show our video
 const showChat = document.querySelector("#showChat");
 const backBtn = document.querySelector(".header__back");
-myVid.muted = true;
+myVid.muted = true;// user mute themselves so that they don't hear themselves back
 
 
 
@@ -25,13 +25,13 @@ backBtn.addEventListener("click", () => {
 });
 
 const user = prompt("Enter your name");
-
+//create peer representing current user
 var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "443",
 });
-
+//accessing user's video and audio stream
 let myVidStream;
 navigator.mediaDevices
   .getUserMedia({
@@ -41,13 +41,13 @@ navigator.mediaDevices
   })
   .then((stream) => {
     myVidStream = stream;
-    addVideoStream(myVid, stream);
+    addVideoStream(myVid, stream);//display user video to themselves
 
-    peer.on("call", (call) => {
-      call.answer(stream);
+    peer.on("call", (call) => {//upon joining someone's room we receive a call from them
+      call.answer(stream);//stream user's audio and video in the room
       const video = document.createElement("video");
       call.on("stream", (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
+        addVideoStream(video, userVideoStream);//diplay other's videos to the user
       });
     });
 
@@ -55,13 +55,18 @@ navigator.mediaDevices
       connectToNewUser(userId, stream);
     });
   });
-
+//when someone joins our room
 const connectToNewUser = (userId, stream) => {
-  const call = peer.call(userId, stream);
+  const call = peer.call(userId, stream);//call the user who joined
   const video = document.createElement("video");
   call.on("stream", (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
+
+  //Remove video when they leave
+  call.on('close',()=>{
+    video.remove();
+  })
 };
 
 peer.on("open", (id) => {
@@ -70,9 +75,9 @@ peer.on("open", (id) => {
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
+  video.addEventListener("loadedmetadata", () => {//play the video as it loads
     video.play();
-    vidGrid.append(video);
+    vidGrid.append(video);//add video element to grid
   });
 };
 
@@ -80,14 +85,14 @@ let txt = document.querySelector("#chat_message");
 let send = document.getElementById("send");
 let messages = document.querySelector(".messages");
 
-send.addEventListener("click", (e) => {
-  if (txt.value.length !== 0) {
+send.addEventListener("click", (e) => {//send message using button
+  if (txt.value.length !== 0) {//send message in chat area only if it is non-empty
     socket.emit("message", txt.value);
     txt.value = "";
   }
 });
 
-txt.addEventListener("keydown", (e) => {
+txt.addEventListener("keydown", (e) => {//send message by pressing Enter key
   if (e.key === "Enter" && txt.value.length !== 0) {
     socket.emit("message", txt.value);
     txt.value = "";
@@ -100,10 +105,10 @@ const muteBtn = document.querySelector("#muteButton");
 muteButton.addEventListener("click", () => {
   const enabled = myVidStream.getAudioTracks()[0].enabled;
   if (enabled) {
-    myVidStream.getAudioTracks()[0].enabled = false;
+    myVidStream.getAudioTracks()[0].enabled = false;//if audio stream is currently enabled then disable it
     html = `<span class="muteDiv"><i class="fas fa-microphone-slash"></i></span>`
      
-    muteBtn.classList.toggle("bg__red");
+    muteBtn.classList.toggle("bg__red");//turn the mute button to red colour
     muteBtn.innerHTML = html;
   } else {
     myVidStream.getAudioTracks()[0].enabled = true;
@@ -132,28 +137,30 @@ stopVideo.addEventListener("click", () => {
   }
 });
 
-const inviteBtn = document.querySelector("#inviteButton");
 
-inviteBtn.addEventListener("click", (e) => {
-  prompt(
-    "Copy the room ID to share with people you want to meet",
-    ROOM_ID
-  );
-});
 
-function okBye(){
+function okBye(){//redirect to homepage when leaving the meet
   const wrning=confirm("Are you sure you wanna leave the meeting!?");
   if(wrning==true){
     window.location.href=`/..`;
   }
 };
 
+const inviteBtn = document.querySelector("#inviteButton");
+
+inviteBtn.addEventListener("click", (e) => {
+  prompt(//show a prompt containing the room ID
+    "Copy the room ID to share with people you want to meet",
+    ROOM_ID
+  );
+});
+
 socket.on("createMessage", (message, userName) => {
   messages.innerHTML =
     messages.innerHTML +
     `<div class="message">
         <b><i class="far fa-user"></i> <span> ${
-          userName === user ? "me" : userName
+          userName === user ? "You" : userName
         }</span> </b>
         <span>${message}</span>
     </div>`;
